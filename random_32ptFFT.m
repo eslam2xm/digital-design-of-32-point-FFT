470dffca3a89a998cd33f5e3dc0a7b8324692fb2
eslam2xm <emamdouh123@gmail.com>
2021-06-04 00:05:54 +0200

    *WIP* added a short matlab script which is used to generate a random stream of 8 bit signed integers and then fed to 32FFT block, this is meant to be used in the future for testing purposes the outputs of this script generates 2 .txt files; input.txt and output.txt in these two files you can find the input stream and output of the FFT listed each output of input in a separate line

--- random_32ptFFT.m
+++ random_32ptFFT.m
@@ -2,4 +2,13 @@ clear; clc;
 
 x = randi([0 127],1,32); %generate the signal
 
-Y = fft(x,32); %Compute the Fourier transform of the signal. 
+y = fft(x,32); %Compute the Fourier transform of the signal. 
+
+%dump this data into external files
+inputFile = fopen('input.txt','w');
+fprintf(inputFile,'%d\n',x);
+fclose(inputFile);
+
+outputFile = fopen('output.txt','w');
+fprintf(outputFile,'%d\n',y);
+fclose(outputFile);
