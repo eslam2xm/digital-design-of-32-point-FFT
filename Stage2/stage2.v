0a7ba9d685620c85923fc143d3e2c319de9e93d8
UsamaEmam <usama.emam02@gmail.com>
2021-06-04 22:14:53 +0200

    *WIP* modify the MAC interface names and new parameters add test bench file for Stage2

--- Stage2/stage2.v
+++ Stage2/stage2.v
@@ -6,8 +6,10 @@ there are 32 outputs from c0 to c31
 */
 
 module Stage2 #(    parameter p_inputBits=16,
-                    parameter p_outputBits=20,
-                    parameter p_widdleBits=4
+                    parameter p_outputBits=28,
+                    parameter p_widdleBits=16,
+                    parameter p_PointPosition=3,
+                    parameter p_realBits = 8
 
             )
             (   
@@ -88,121 +90,117 @@ module Stage2 #(    parameter p_inputBits=16,
                 output  [p_outputBits-1:0]  o_c31  
             );
 
-MAC  #(  .p_inputMacBits(p_inputBits),
-            .p_outputMacBits(p_outputBits),
-            .p_widdleMacBits(p_widdleBits)  
+sharedComplexMUL  #(  .p_inputWidth(p_realBits),
+            .p_PointPosition(p_PointPosition)  
             )
 mac0 (   .CLK(CLK),
         .RST(RST),
-        .MAca0(i_b0),
-        .MAca1(i_b1),
-        .MAca2(i_b2),
-        .MAca3(i_b3),
-        .MAcb0(i_b4),
-        .MAcb1(i_b5),
-        .MAcb2(i_b6),
-        .MAcb3(i_b7),
-        .MAcc0(i_w08), 
-        .MAcc1(i_w18),
-        .MAcc2(i_w28),
-        .MAcc3(i_w38),
-        .MAcOut0(o_c0),
-        .MAcOut1(o_c4),
-        .MAcOut2(o_c1),
-        .MAcOut3(o_c5),
-        .MAcOut4(o_c2),
-        .MAcOut5(o_c6),
-        .MAcOut6(o_c3),
-        .MAcOut7(o_c7)
+        .i_m1(i_b0),
+        .i_m2(i_b1),
+        .i_m3(i_b2),
+        .i_m4(i_b3),
+        .i_n1(i_b4),
+        .i_n2(i_b5),
+        .i_n3(i_b6),
+        .i_n4(i_b7),
+        .i_l1(i_w08), 
+        .i_l2(i_w18),
+        .i_l3(i_w28),
+        .i_l4(i_w38),
+        .o_r1_p(o_c0),
+        .o_r1_m(o_c4),
+        .o_r2_p(o_c1),
+        .o_r2_m(o_c5),
+        .o_r3_p(o_c2),
+        .o_r3_m(o_c6),
+        .o_r4_p(o_c3),
+        .o_r4_m(o_c7)
 );
 
-MAC  #(  .p_inputMacBits(p_inputBits),
-            .p_outputMacBits(p_outputBits),
-            .p_widdleMacBits(p_widdleBits)  
+sharedComplexMUL  #(  .p_inputWidth(p_realBits),
+            .p_PointPosition(p_PointPosition)  
             )
 
 mac1 (   .CLK(CLK),
         .RST(RST),
-        .MAca0(i_b8),
-        .MAca1(i_b9),
-        .MAca2(i_b10),
-        .MAca3(i_b11),
-        .MAcb0(i_b12),
-        .MAcb1(i_b13),
-        .MAcb2(i_b14),
-        .MAcb3(i_b15),
-        .MAcc0(i_w08), 
-        .MAcc1(i_w18),
-        .MAcc2(i_w28),
-        .MAcc3(i_w38),
-        .MAcOut0(o_c8),
-        .MAcOut1(o_c12),
-        .MAcOut2(o_c9),
-        .MAcOut3(o_c13),
-        .MAcOut4(o_c10),
-        .MAcOut5(o_c14),
-        .MAcOut6(o_c11),
-        .MAcOut7(o_c15)
+        .i_m1(i_b8),
+        .i_m2(i_b9),
+        .i_m3(i_b10),
+        .i_m4(i_b11),
+        .i_n1(i_b12),
+        .i_n2(i_b13),
+        .i_n3(i_b14),
+        .i_n4(i_b15),
+        .i_l1(i_w08), 
+        .i_l2(i_w18),
+        .i_l3(i_w28),
+        .i_l4(i_w38),
+        .o_r1_p(o_c8),
+        .o_r1_m(o_c12),
+        .o_r2_p(o_c9),
+        .o_r2_m(o_c13),
+        .o_r3_p(o_c10),
+        .o_r3_m(o_c14),
+        .o_r4_p(o_c11),
+        .o_r4_m(o_c15)
 );
 
 
-MAC  #(  .p_inputMacBits(p_inputBits),
-            .p_outputMacBits(p_outputBits),
-            .p_widdleMacBits(p_widdleBits)  
+sharedComplexMUL  #(  .p_inputWidth(p_realBits),
+            .p_PointPosition(p_PointPosition)  
             )
 
 mac2 (   .CLK(CLK),
         .RST(RST),
-        .MAca0(i_b16),
-        .MAca1(i_b17),
-        .MAca2(i_b18),
-        .MAca3(i_b19),
-        .MAcb0(i_b20),
-        .MAcb1(i_b21),
-        .MAcb2(i_b22),
-        .MAcb3(i_b23),
-        .MAcc0(i_w08), 
-        .MAcc1(i_w18),
-        .MAcc2(i_w28),
-        .MAcc3(i_w38),
-        .MAcOut0(o_c16),
-        .MAcOut1(o_c20),
-        .MAcOut2(o_c17),
-        .MAcOut3(o_c21),
-        .MAcOut4(o_c18),
-        .MAcOut5(o_c22),
-        .MAcOut6(o_c19),
-        .MAcOut7(o_c23)
+        .i_m1(i_b16),
+        .i_m2(i_b17),
+        .i_m3(i_b18),
+        .i_m4(i_b19),
+        .i_n1(i_b20),
+        .i_n2(i_b21),
+        .i_n3(i_b22),
+        .i_n4(i_b23),
+        .i_l1(i_w08), 
+        .i_l2(i_w18),
+        .i_l3(i_w28),
+        .i_l4(i_w38),
+        .o_r1_p(o_c16),
+        .o_r1_m(o_c20),
+        .o_r2_p(o_c17),
+        .o_r2_m(o_c21),
+        .o_r3_p(o_c18),
+        .o_r3_m(o_c22),
+        .o_r4_p(o_c19),
+        .o_r4_m(o_c23)
 );
 
 
-MAC  #(  .p_inputMacBits(p_inputBits),
-            .p_outputMacBits(p_outputBits),
-            .p_widdleMacBits(p_widdleBits)  
+sharedComplexMUL  #(  .p_inputWidth(p_realBits),
+            .p_PointPosition(p_PointPosition)  
             )
 
 mac3 (   .CLK(CLK),
         .RST(RST),
-        .MAca0(i_b24),
-        .MAca1(i_b25),
-        .MAca2(i_b26),
-        .MAca3(i_b27),
-        .MAcb0(i_b28),
-        .MAcb1(i_b29),
-        .MAcb2(i_b30),
-        .MAcb3(i_b31),
-        .MAcc0(i_w08), 
-        .MAcc1(i_w18),
-        .MAcc2(i_w28),
-        .MAcc3(i_w38),
-        .MAcOut0(o_c24),
-        .MAcOut1(o_c28),
-        .MAcOut2(o_c25),
-        .MAcOut3(o_c29),
-        .MAcOut4(o_c26),
-        .MAcOut5(o_c30),
-        .MAcOut6(o_c27),
-        .MAcOut7(o_c31)
+        .i_m1(i_b24),
+        .i_m2(i_b25),
+        .i_m3(i_b26),
+        .i_m4(i_b27),
+        .i_n1(i_b28),
+        .i_n2(i_b29),
+        .i_n3(i_b30),
+        .i_n4(i_b31),
+        .i_l1(i_w08), 
+        .i_l2(i_w18),
+        .i_l3(i_w28),
+        .i_l4(i_w38),
+        .o_r1_p(o_c24),
+        .o_r1_m(o_c28),
+        .o_r2_p(o_c25),
+        .o_r2_m(o_c29),
+        .o_r3_p(o_c26),
+        .o_r3_m(o_c30),
+        .o_r4_p(o_c27),
+        .o_r4_m(o_c31)
 );
 
 endmodule
---
+++ Stage2/tb_Stage2.v
@@ -0,0 +1,108 @@
+`timescale 1ns / 1ps
+
+
+module tb_Stage2#(  parameter p_inputBits=16,
+                    parameter p_outputBits=28,
+                    parameter p_widdleBits=16,
+                    parameter p_PointPosition=3,
+                    parameter p_realBits = 8
+            );
+
+reg [p_inputBits-1:0]  i_b [31:0];
+reg [p_widdleBits-1:0]  i_w[3:0];
+reg CLK;
+reg RST;
+
+wire [p_outputBits-1:0]  o_c[31:0];
+
+
+Stage2 #(   .p_inputBits        (p_inputBits),
+            .p_outputBits       ( p_outputBits),
+            .p_widdleBits       ( p_widdleBits),
+            .p_PointPosition    ( p_PointPosition),
+            .p_realBits         (p_realBits)
+            )
+            stage_2
+            (   
+                
+                ///// clock and reset ///////
+                .CLK(CLK),
+                .RST(RST),
+                //// inputs to the stage //////////
+                .i_b0(i_b[0])  ,
+                .i_b1(i_b[1])  ,
+                .i_b2(i_b[2])  ,
+                .i_b3(i_b[3])  ,
+                .i_b4(i_b[4])  ,
+                .i_b5(i_b[5])  ,
+                .i_b6(i_b[6])  ,
+                .i_b7(i_b[7])  ,
+                .i_b8(i_b[8])  ,
+                .i_b9(i_b[9])  ,
+                .i_b10(i_b[10])  ,
+                .i_b11(i_b[11])  ,
+                .i_b12(i_b[12])  ,
+                .i_b13(i_b[13])  ,
+                .i_b14(i_b[14])  ,
+                .i_b15(i_b[15])  ,
+                .i_b16(i_b[16])  ,
+                .i_b17(i_b[17])  ,
+                .i_b18(i_b[18])  ,
+                .i_b19(i_b[19])  ,
+                .i_b20(i_b[20])  ,
+                .i_b21(i_b[21])  ,
+                .i_b22(i_b[22])  ,
+                .i_b23(i_b[23])  ,
+                .i_b24(i_b[24])  ,
+                .i_b25(i_b[25])  ,
+                .i_b26(i_b[26])  ,
+                .i_b27(i_b[27])  ,
+                .i_b28(i_b[28])  ,
+                .i_b29(i_b[29])  ,
+                .i_b30(i_b[30])  ,
+                .i_b31(i_b[31])  ,
+                ////////// Widdle at the stage ///////
+                .i_w08(i_w[0]),
+                .i_w18(i_w[1]),
+                .i_w28(i_w[2]),
+                .i_w38(i_w[3]),
+                ////// Outputs from the stage ///////
+                .o_c0(o_c[0]),
+                .o_c1(o_c[1])  ,
+                .o_c2(o_c[2])  ,
+                .o_c3(o_c[3])  ,
+                .o_c4(o_c[4])  ,
+                .o_c5(o_c[5])  ,
+                .o_c6(o_c[6])  ,
+                .o_c7(o_c[7])  ,
+                .o_c8(o_c[8])  ,
+                .o_c9(o_c[9])  ,
+                .o_c10(o_c[10])  ,
+                .o_c11(o_c[11])  ,
+                .o_c12(o_c[12])  ,
+                .o_c13(o_c[13])  ,
+                .o_c14(o_c[14])  ,
+                .o_c15(o_c[15])  ,
+                .o_c16(o_c[16])  ,
+                .o_c17(o_c[17])  ,
+                .o_c18(o_c[18])  ,
+                .o_c19(o_c[19])  ,
+                .o_c20(o_c[20])  ,
+                .o_c21(o_c[21])  ,
+                .o_c22(o_c[22])  ,
+                .o_c23(o_c[23])  ,
+                .o_c24(o_c[24])  ,
+                .o_c25(o_c[25])  ,
+                .o_c26(o_c[26])  ,
+                .o_c27(o_c[27])  ,
+                .o_c28(o_c[28])  ,
+                .o_c29(o_c[29])  ,
+                .o_c30(o_c[30])  ,
+                .o_c31(o_c[31])  
+            );
+
+
+
+
+
+endmodule
